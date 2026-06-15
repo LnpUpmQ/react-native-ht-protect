56ae9f7cf671463f1b5422b8e2c8116e12807069
zhangcj <zcjan@sina.com>
2026-06-15 17:33:43 +0800

    refactor(NativeHtProtect): 更新模块接口定义以支持新功能
    - 添加 RequestCmdID 枚举用于初始化配置和服务端响应传递
    - 修改 init 方法返回类型为包含 code 和 message 的对象
    - 添加 ioctl 方法用于处理命令请求
    - 更新接口定义以支持更完整的错误处理机制

--- src/NativeHtProtect.ts
+++ src/NativeHtProtect.ts
@@ -17,12 +17,24 @@ export interface TRoleInfo {
   gameJson: string | undefined;
 }
 
+export enum RequestCmdID {
+  //初始化配置内容，防止初始化失败情况下，客户可以通过服务端请求初始化配置并传递给SDK
+  Cmd_SetConfigData = 16,
+  //客户服务端将check结果传递给SDK，便于执行后续动作
+  Cmd_SetResponseData = 17,
+}
+
 export interface Spec extends TurboModule {
-  init(productId: string, config: TConfig): Promise<void>;
+  init(
+    productId: string,
+    config: TConfig
+  ): Promise<{ code: number; message: string }>;
 
   setRoleInfo(businessId: string, info: TRoleInfo): Promise<void>;
 
   getTokenAsync(businessId: string, timeout: number): Promise<string>;
+
+  ioctl(request: RequestCmdID, data?: string): Promise<void>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('HtProtect');
